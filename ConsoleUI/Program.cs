f195e9691497423c37fa5b9188841b7f78537556
Emin Tatar <emintatar48@outlook.com>
2023-05-23 12:13:17 +0300

    first commit

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,25 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ICarService carManager = new CarManager(new InMemoryCarDal());
+
+            List<Car> cars = carManager.GetAll();
+
+            foreach (Car car in cars)
+            {
+                Console.WriteLine(car.Id + " " + car.Description);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Car rental system.
+
+### - Create a brand new project. Its name will be CarRental.
+
+### - Create Entities, DataAccess, Business and Console layers.
+
+### - Create a "Car" object. 
+
+### - As property: Add Id, BrandId, ColorId, ModelYear, DailyPrice, Description fields.
+
+### - Write the GetById, GetAll, Add, Update, Delete operations in InMemory format.
+
+### - Test it in console.
